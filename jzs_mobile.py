2f4150675ebcb9efc6f8e84f156a0e5e68b16697
cloud <cloudcry@gmail.com>
2012-03-30 16:13:18 +0800

    config change

--- jzs_mobile.py
+++ jzs_mobile.py
@@ -113,5 +113,5 @@ def detail(eid):
 
 
 if __name__ == "__main__":
-    http_server = WSGIServer(('127.0.0.1', 8100), app)
+    http_server = WSGIServer(('127.0.0.1', 8300), app)
     http_server.serve_forever()
